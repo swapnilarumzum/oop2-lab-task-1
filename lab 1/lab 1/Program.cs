e69520a2a5fdcd631c9d4af5215fe612b618180a
swapnilarumzum <swapnilarumzum@gmail.com>
2020-11-03 02:26:47 -0800

    lab task 1 complited

---
+++ lab 1/lab 1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*//prob 01...............
+            Console.WriteLine("Hello");*/
+            /* //prob 02..............
+             int a, b,sum;
+             a = 5;
+             b = 15;
+             sum = a + b;
+             Console.WriteLine("The Sum of two number A and B is: " + sum);
+             Console.ReadLine();*/
+            /* //prob 03...................
+             int a, b, div;
+             a = 5;
+             b = 15;
+             div = b / a;
+             Console.WriteLine("The Dividation of two number A and B is: " + div);
+             Console.ReadLine();*/
+
+            /*//prob 04.............
+            int a = (-5 + 8 * 6);
+            int b = ((55 + 9) % 9);
+            int c = (20 + -3 * 5 / 8);
+            int d = (5 + 15 / 3 * 2 - 8 % 3);
+            Console.WriteLine("The answer of A: "+a);
+            Console.WriteLine("The answer of b: " + b);
+            Console.WriteLine("The answer of c: " + c);
+            Console.WriteLine("The answer of d: " + d);
+            Console.ReadLine();*/
+            /*//prob 05................
+            int a, b,add,mul,sub, div;
+            a = 5;
+            b = 15;
+            add = a + b;
+            mul = a * b;
+            sub = b - a;
+            div = b / a;
+            Console.WriteLine("The Sum of two number A and B is: " + add);
+            Console.WriteLine("The Multipication of two number A and B is: " + mul);
+            Console.WriteLine("The substraction of two number A and B is: " + sub);
+            Console.WriteLine("The Dividation of two number A and B is: " + div);
+            Console.ReadLine();*/
+            /* //prob 06...........
+             double e = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+             Console.WriteLine("compute the specified expressions is: " + e);
+             Console.WriteLine();
+            */
+            /* //prob07................
+            double Width = 5.5, Height = 8.5;
+             double Perimeter = (2 * (Width + Height));
+              double   Area = Width * Height;
+
+             Console.WriteLine("Area is 5.6 * 8.5 =" +Area);
+             Console.WriteLine("Perimeter is 2 * (5.6 + 8.5) =" + Perimeter);
+             Console.ReadLine();*/
+            /*//prb 08.............
+            int firstinteger= 25;
+             int secondinteger= 39;
+             Console.WriteLine("25 != 39 \n25 < 39 \n25 <= 39");
+             Console.ReadLine();*/
+            /*  //prb 09.............
+              string s = "Reverse";
+              int i = 7;
+              Console.WriteLine("The string is: "+s);
+              Console.WriteLine("The string in Reverse: " );
+              Console.Write(s[i]);
+              Console.ReadLine();*/
+            /* //prb 10.............
+             double a;
+             double b;
+             bool between = true;
+             bool notbetween = false;
+             Console.WriteLine("Input a double value: ");
+             a = Convert.ToDouble( Console.ReadLine());
+             Console.WriteLine("Input another double value: ");
+             b = Convert.ToDouble(Console.ReadLine());
+             if (a > 0 && a < 1 && b > 0 && b < 1)
+             {
+                 Console.WriteLine(between);
+
+             }
+
+             else { Console.WriteLine(notbetween); }
+             Console.ReadLine();*/
+        }
+    }
+}
